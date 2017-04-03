8a7e06cfc99c791f1e25744b98795a98957cc633
bonford <bonford@users.noreply.github.com>
2017-04-03 02:07:21 -0400

    sockets set up, working on messages

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+})
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+
+    })
+
+    // Send Message
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data});
+    })
+
+
+
 })
